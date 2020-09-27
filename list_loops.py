c6be4f737a0c753fbe9c55d72dee56dbf95f5bde
Ben Chan <benchan777@gmail.com>
2020-09-26 22:22:53 -0700

    comments

--- list_loops.py
+++ list_loops.py
@@ -1,4 +1,9 @@
 songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+# prints first element of song list
 print(songs[0])
+# prints last element of song list
 print(songs[2])
+#prints second and third element of song list
 print(songs[1:3])
+
